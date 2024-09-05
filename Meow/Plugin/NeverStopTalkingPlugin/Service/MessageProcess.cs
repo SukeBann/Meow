b09726d6b4352cd6ea95ce8006698e607ba2bd9d
yangzhenyu <1052700448@qq.com>
2024-09-05 14:20:27 +0800

    重构触发器逻辑以简化消息处理
    将触发逻辑提取并重构为专用方法“TryToTrigger”，提高代码清晰度和可维护性。此外，将触发概率从 8/1000 调整为 66/1000，并删除了未使用的“IsForceTrigger”属性。

--- Meow/Plugin/NeverStopTalkingPlugin/Service/MessageProcess.cs
+++ Meow/Plugin/NeverStopTalkingPlugin/Service/MessageProcess.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using JiebaNet.Segmenter;
 using Lagrange.Core.Message;
 using Lagrange.Core.Message.Entity;
@@ -9,19 +10,19 @@ using Meow.Utils;
 
 namespace Meow.Plugin.NeverStopTalkingPlugin.Service;
 
-/// <summary>
-/// 消息处理, 接收插件处理到的消息
-/// <br/> 先查询该消息的文本是否被处理过(数据库md5对比), 如果处理过就不再响应（防复读机）,
-/// <br/> 先进行屏蔽词查询, 如果有屏蔽词则不处理该消息
-/// <br/> 将消息分词, 从中筛取掉所有停用词, 判断一个合适的大小， 比如分词结果要大于两个以上才存储
-/// <br/>
-/// <br/> 消息处理器会把分词结果与消息源放入词袋处理器中, 处理器会根据词袋类型处理消息, 并返回消息向量
-/// <br/> 词袋会有两种类型 一种为群聊词袋, 容量相对来说较大, 收集的是所有人的消息
-/// <br/> 另外一种是个人词袋, 容量较小, 收集的是个人的消息, 对于群聊词袋来说 更能体现个人说话风格, 但是如果聊天消息过少， 可能词袋得花很久才会生成
-/// <br/>
-/// <br/> 词袋处理器会返回一个结果, 如果词袋未被填充完成 则该消息只会被存储, 并且用于扩充词袋 不会被响应
-/// <br/> 否则词袋会返回一条消息向量, 消息处理器可以使用消息向量去查找最相似的消息
-/// </summary>
+        /// <summary>
+        /// 消息处理, 接收插件处理到的消息
+        /// <br/> 先查询该消息的文本是否被处理过(数据库md5对比), 如果处理过就不再响应（防复读机）,
+        /// <br/> 先进行屏蔽词查询, 如果有屏蔽词则不处理该消息
+        /// <br/> 将消息分词, 从中筛取掉所有停用词, 判断一个合适的大小， 比如分词结果要大于两个以上才存储
+        /// <br/>
+        /// <br/> 消息处理器会把分词结果与消息源放入词袋处理器中, 处理器会根据词袋类型处理消息, 并返回消息向量
+        /// <br/> 词袋会有两种类型 一种为群聊词袋, 容量相对来说较大, 收集的是所有人的消息
+        /// <br/> 另外一种是个人词袋, 容量较小, 收集的是个人的消息, 对于群聊词袋来说 更能体现个人说话风格, 但是如果聊天消息过少， 可能词袋得花很久才会生成
+        /// <br/>
+        /// <br/> 词袋处理器会返回一个结果, 如果词袋未被填充完成 则该消息只会被存储, 并且用于扩充词袋 不会被响应
+        /// <br/> 否则词袋会返回一条消息向量, 消息处理器可以使用消息向量去查找最相似的消息
+        /// </summary>
 public class MessageProcess : HostDatabaseSupport
 {
     /// <inheritdoc />
@@ -58,20 +59,15 @@ public class MessageProcess : HostDatabaseSupport
     private TextCutter TextCutter { get; set; }
     
     /// <summary>
-    /// 触发几率千分数 如果这个值是8 那么触发几率就是 8/1000
+    /// 触发几率千分数 如果这个值是18 那么触发几率就是 18/1000
     /// </summary>
-    private int TriggerProbabilityPerThousand { get; set; } = 8;
+    private int TriggerProbabilityPerThousand { get; set; } = 66;
 
     /// <summary>
     /// 触发几率随机数获取器
     /// </summary>
     private readonly Random _triggerRandom = new();
     
-    /// <summary>
-    /// 是否要强制触发
-    /// </summary>
-    private bool IsForceTrigger { get; set; }
-
     /// <summary>
     /// 触发记录，包含与消息分词和触发条件相关的所有记录
     /// </summary>
@@ -134,30 +130,8 @@ public class MessageProcess : HostDatabaseSupport
     {
         result = (0, null);
         
-        // 强制触发
-        var isForceTrigger = false;
-        
-        var randomNum = _triggerRandom.Next(0, 1000);
-        var bagOfWordType = msgRecord.IsGroupMsg ? BagOfWordType.Group : BagOfWordType.Personal;
-        var uin = msgRecord.IsGroupMsg ? msgRecord.GroupId : msgRecord.Sender;
-        var nstTriggerRecord = TriggerRecords.FirstOrDefault(x => x.Uin == uin && x.BagOfWordType == bagOfWordType);
-        if (nstTriggerRecord is null)
+        if (!TryToTrigger(msgRecord, out var nstTriggerRecord))
         {
-            nstTriggerRecord = new NstTriggerRecord(bagOfWordType, uin, 0, default);
-            TriggerRecords.Add(nstTriggerRecord);
-        }
-        else
-        {
-            if (nstTriggerRecord.TriggerFailedCount > 10)
-            {
-                Host.Info($"{nstTriggerRecord.Uin}-{nstTriggerRecord.TriggerFailedCount}: isForceTrigger: true");
-                isForceTrigger = true;
-            }
-        }
-        
-        if (!isForceTrigger && randomNum > TriggerProbabilityPerThousand)
-        {
-            Host.Info($"return: {randomNum}, {nstTriggerRecord.TriggerFailedCount}");
             return false;
         }
 
@@ -188,6 +162,59 @@ public class MessageProcess : HostDatabaseSupport
         result = (similarity, firstMsg);
         nstTriggerRecord.TriggerFailedCount = 0;
         nstTriggerRecord.LastTriggered = DateTime.Now;
+        return true;
+    }
+
+    /// <summary>
+    /// 尝试触发操作。
+    /// </summary>
+    /// <param name="msgRecord">消息记录对象。</param>
+    /// <param name="nstTriggerRecord">触发记录输出参数。</param>
+    /// <returns>如果触发成功，返回 true；否则返回 false。</returns>
+    /// <remarks>
+    /// 该方法根据消息记录和一定的几率（以及强制触发条件）决定是否触发某一操作。
+    /// </remarks>
+    private bool TryToTrigger(MsgRecord msgRecord, [MaybeNullWhen(false)]out NstTriggerRecord nstTriggerRecord)
+    {
+        // 初始化'强制触发'标志，默认为false
+        var isForceTrigger = false;
+
+        // 生成一个介于0和1000之间的随机数
+        var randomNum = _triggerRandom.Next(0, 1000);
+
+        // 根据消息类型设置词袋类型
+        var bagOfWordType = msgRecord.IsGroupMsg ? BagOfWordType.Group : BagOfWordType.Personal;
+
+        // 根据消息类型获取唯一标识符（群ID或发送者ID）
+        var uin = msgRecord.IsGroupMsg ? msgRecord.GroupId : msgRecord.Sender;
+
+        // 查找与当前消息对应的触发记录
+        nstTriggerRecord = TriggerRecords.FirstOrDefault(x => x.Uin == uin && x.BagOfWordType == bagOfWordType);
+
+        // 如果没有找到对应的触发记录，则创建一个新的记录并添加到列表中
+        if (nstTriggerRecord is null)
+        {
+            nstTriggerRecord = new NstTriggerRecord(bagOfWordType, uin, 0, default);
+            TriggerRecords.Add(nstTriggerRecord);
+        }
+        else
+        {
+            // 如果触发失败次数大于等于3次，则强制触发
+            if (nstTriggerRecord.TriggerFailedCount >= 3)
+            {
+                Host.Info($"{nstTriggerRecord.Uin}-{nstTriggerRecord.TriggerFailedCount}: isForceTrigger: true");
+                isForceTrigger = true;
+            }
+        }
+
+        // 如果不是强制触发，且随机数大于触发概率阈值，则返回false，表示不触发
+        if (!isForceTrigger && randomNum > TriggerProbabilityPerThousand)
+        {
+            Host.Info($"return: {randomNum}, {nstTriggerRecord.TriggerFailedCount}");
+            return false;
+        }
+
+        // 返回true，表示触发
         return true;
     }
 
