937fe1769c52d34f8bf70c4d6e313c88105f3b05
yangzhenyu <1052700448@qq.com>
2024-09-28 17:11:25 +0800

    添加 UsagiPlugin 和初始音频资源
    引入了 UsagiPlugin，使 Bot 能够与 Usagi 的声音和表情进行交互。添加了初始音频资源并设置了用于消息处理和资源管理的插件结构。修改了项目文件以包含 UsagiPlugin 资产所需的文件夹。

---
+++ Meow/Plugin/UsagiPlugin/UsagiPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Lagrange.Core.Message;
+using Meow.Core.Model.Base;
+
+namespace Meow.Plugin.UsagiPlugin;
+
+/// <summary>
+/// 使Bot发出Usagi的声音和表情包
+/// </summary>
+public class UsagiPlugin: PluginBase
+{
+    /// <inheritdoc />
+    public override string PluginName => "Usagi";
+
+    /// <inheritdoc />
+    public override string PluginDescription => "使Bot发出Usagi的声音和表情包";
+
+    /// <inheritdoc />
+    public override string PluginUid => "3D78339D-2E7B-4AB7-B046-C5C24193A84B";
+    
+    /// <summary>
+    /// 取消消息订阅
+    /// </summary>
+    private IDisposable? MsgSubscribeDisposable { get; set; }
+
+    /// <inheritdoc />
+    public override void InjectPlugin(Core.Meow host)
+    {
+        base.InjectPlugin(host);
+        host.OnMessageReceived.ObserveOn(ThreadPoolScheduler.Instance).Subscribe(Handle);
+    }
+
+    private void Handle((Core.Meow meow, MessageChain messageChain, MessageChain.MessageType messageType) _)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public override void Remove()
+    {
+        base.Remove();
+        MsgSubscribeDisposable?.Dispose();
+    }
+}
---
+++ Meow/Plugin/UsagiPlugin/UsagiResourceManager.cs
@@ -0,0 +1,37 @@
+﻿namespace Meow.Plugin.UsagiPlugin;
+
+/// <summary>
+/// 资源管理
+/// </summary>
+public class UsagiResourceManager
+{
+    /// <summary>
+    /// 肯定语气音频
+    /// </summary>
+    private string[] SureAudio { get; set; }
+    
+    /// <summary>
+    /// 疑问语气音频
+    /// </summary>
+    private string[] QuestionableAudio { get; set; }
+    
+    /// <summary>
+    /// 发疯音频
+    /// </summary>
+    private string[] CrazyAudio { get; set; }
+    
+    /// <summary>
+    /// 表示肯定的表情包id
+    /// </summary>
+    private string[] SureImageUid { get; set; }
+    
+    /// <summary>
+    /// 表示疑问的表情包id
+    /// </summary>
+    private string[] QuestionableImageUid { get; set; }
+    
+    /// <summary>
+    /// 表示发疯的表情包id
+    /// </summary>
+    private string[] CrazyImageUid { get; set; }
+}
